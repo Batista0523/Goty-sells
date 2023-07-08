4f6cbf4dcea75f2163cd4823819099716cd298cd
Elisaul Batista <elisaulbatista@Elisauls-MacBook-Air.local>
2023-07-07 23:04:35 -0400

    now fixed if the user enter a string on the price section will show a error msj that alert the user they must use  number

--- main.js
+++ main.js
@@ -1,25 +1,23 @@
 const unitForm = document.getElementById('unitForm');
 const unitList = document.getElementById('unitList');
 
-// Event listener for form submission
 unitForm.addEventListener('submit', function(event) {
   event.preventDefault();
 
-  const nameInput = document.getElementById('name');
-  const availabilityInput = document.getElementById('availability');
-  const priceInput = document.getElementById('price');
-  const stockInput = document.getElementById('stock');
+  const { value: name } = document.getElementById('name');
+  const { value: availability } = document.getElementById('availability');
+  const { value: price } = document.getElementById('price');
+  const { value: stock } = document.getElementById('stock');
 
-  const name = nameInput.value;
-  const availability = availabilityInput.value;
-  const price = priceInput.value;
-  const stock = stockInput.value;
+  if (!isNumeric(price)) {
+    displayError('Price must be a number', 'price');
+    return;
+  }
 
   const imageUrl = getImageUrl(availability);
 
   const newUnit = document.createElement('div');
   newUnit.classList.add('unit');
-
   newUnit.innerHTML = `
     <h2>${name}</h2>
     <h4>${availability}</h4>
@@ -31,33 +29,40 @@ unitForm.addEventListener('submit', function(event) {
 
   unitList.appendChild(newUnit);
 
-  nameInput.value = '';
-  availabilityInput.selectedIndex = 0;
-  priceInput.value = '';
-  stockInput.selectedIndex = 0;
-
-  // Add event listener to the new "Remove" button
-  const removeButton = newUnit.querySelector('.remove');
-  removeButton.addEventListener('click', function() {
-    unitList.removeChild(newUnit);
-  });
+  unitForm.reset();
+  clearError('price');
 });
 
-function getImageUrl(availability) {
-  if (availability === 'M-Series H2i') {
-    return 'https://www.acwholesalers.com/products-image/208/mass_107672_.jpg';
-  } else if (availability === 'M-Series 115V') {
-    return 'https://www.acwholesalers.com/products-image/600/MZ-JP12WA_99497_600.png';
-  } else if (availability === 'HM-Series') {
-    return 'https://www.acwholesalers.com/products-image/600/mass_81586_600.png';
-  } 
-}
+const getImageUrl = (availability) => {
+  const availabilityMap = {
+    'M-Series H2i': 'https://www.acwholesalers.com/products-image/208/mass_107672_.jpg',
+    'M-Series 115V': 'https://www.acwholesalers.com/products-image/600/MZ-JP12WA_99497_600.png',
+    'HM-Series': 'https://www.acwholesalers.com/products-image/600/mass_81586_600.png',
+  };
 
-// Add event listeners to existing "Remove" buttons
+  return availabilityMap[availability] || '';
+};
+
 const removeButtons = document.querySelectorAll('.remove');
-removeButtons.forEach(function(button) {
-  button.addEventListener('click', function() {
+removeButtons.forEach((button) => {
+  button.addEventListener('click', () => {
     const unit = button.parentNode;
     unitList.removeChild(unit);
   });
 });
+
+const isNumeric = (value) => !isNaN(parseFloat(value)) && isFinite(value);
+
+const displayError = (errorMessage, fieldId) => {
+  const errorElement = document.createElement('p');
+  errorElement.classList.add('error-message');
+  errorElement.innerText = errorMessage;
+
+  const field = document.getElementById(fieldId);
+  field.parentNode.appendChild(errorElement);
+};
+
+const clearError = (fieldId) => {
+  const errorElement = document.querySelector(`#${fieldId} ~ .error-message`);
+  if (errorElement) errorElement.remove();
+};
